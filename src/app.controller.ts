81338c94c24499a5dbe37b16edce263f911409ba
luiz <luizgustavofcsg@hotmail.com>
2021-03-12 11:54:08 -0300

    implementação da geração do bearer token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 import { LocalAuthGuart } from './auth/local.auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService
+  ) {}
 
   @UseGuards(LocalAuthGuart)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { User, UsersService } from 'src/users/users.service';
+
+type AccessTokenObject = {
+    access_token: any;
+}
 
 @Injectable()
 export class AuthService {
 
-    constructor(private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService    
+    ) {}
 
     async validateUser(username: string, password: string): Promise<User | undefined> {
         const user = await this.usersService.findOne(username);
@@ -16,4 +24,8 @@ export class AuthService {
         return null;
     }
 
+    async login(user: User): Promise<AccessTokenObject> {
+        const payload = {username: user.username, sub: user.userId}
+        return { access_token: this.jwtService.sign(payload) }
+    }
 }
